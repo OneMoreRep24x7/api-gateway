0282f8080ba3fd6bdaa10f555b8c2584bdc7729a
Ashish S <ashish@Ashishs-MacBook-Air.local>
2024-03-26 05:45:37 +0530

    Add authentication filter and route validator

---
+++ src/main/java/com/ashish/apigateway/filter/AuthenticationFilter.java
@@ -0,0 +1,55 @@
+package com.ashish.apigateway.filter;
+
+import com.ashish.apigateway.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    @Autowired
+    private RouteValidator validator;
+
+    @Autowired
+    private JwtUtils jwtUtil;
+
+    public AuthenticationFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            if (validator.isSecured.test(exchange.getRequest())) {
+
+                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    throw new RuntimeException("Missing authorization header");
+                }
+
+                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    authHeader = authHeader.substring(7);
+                }
+
+                try {
+                    // Validate JWT token
+                    jwtUtil.validateToken(authHeader);
+                } catch (Exception e) {
+                    // Log the error and return an unauthorized response
+                    System.out.println("Failed to validate token: " + e.getMessage());
+                    exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return exchange.getResponse().setComplete();
+                }
+            }
+            return chain.filter(exchange);
+        });
+    }
+
+    public static class Config {
+        // No configuration parameters needed
+    }
+}
